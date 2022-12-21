dac978aa0823893643738d0d50136ea432f7878f
Paul Mieschke <p.mieschke@gmail.com>
2022-12-21 22:35:01 +0100

    Moon setup done

--- Assets/Scripts/World/DayNightCycle.cs
+++ Assets/Scripts/World/DayNightCycle.cs
@@ -1,19 +1,27 @@
 using UnityEngine;
 
 namespace World {
+    [RequireComponent(typeof(Light))]
     public class DayNightCycle : MonoBehaviour {
         private const int SECONDS_PER_DAY = 86400;
         private const float DEFAULT_START_TIME = 14.00f;
+
+        private Light _light;
 
         [Header("Setup")]
         [SerializeField, Range(0f, 90f)] private float _sunMaxHeight = 55f;
         [SerializeField, Range(0f, 23.99f)] private float _startTime = DEFAULT_START_TIME;
         [SerializeField, Min(0f), Tooltip("86400 (seconds) is real-time")] private int _dayDuration = SECONDS_PER_DAY;
+        [SerializeField, Range(0f, 23.99f)] private float _enableSunLightTime = 05.00f;
+        [SerializeField, Range(0f, 23.99f)] private float _disableSunLightTime = 19.00f;
 
         [field: Header("Runtime")]
         [field: SerializeField, Range(0f, 23.99f)] public float CurrTime { get; set; } = DEFAULT_START_TIME;
 
         private void Awake() {
+            _light = GetComponent<Light>();
+            if (!_light) Debug.LogWarning("DayNightCycle: no light found");
+
             Setup();
         }
 
@@ -24,7 +32,11 @@ namespace World {
 
         private void Update() {
             CurrTime = (CurrTime + (Time.deltaTime / _dayDuration * 24f)) % 24f;
-            UpdateSunPos();
+
+            if (!_light.enabled && CurrTime > _enableSunLightTime && CurrTime < _disableSunLightTime) _light.enabled = true;
+            else if (_light.enabled && CurrTime > _disableSunLightTime) _light.enabled = false;
+
+            if (CurrTime > _enableSunLightTime && CurrTime < _disableSunLightTime) UpdateSunPos();
         }
 
         private void UpdateSunPos() {
