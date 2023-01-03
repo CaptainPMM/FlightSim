343c4b1e9640a076a53f7952471ffa7503214638
Paul Mieschke <p.mieschke@gmail.com>
2023-01-03 01:22:04 +0100

    DayNightCycle time mod and input option

--- Assets/Scripts/World/DayNightCycle.cs
+++ Assets/Scripts/World/DayNightCycle.cs
@@ -17,9 +17,11 @@ namespace World {
         [SerializeField, Min(0f), Tooltip("86400 (seconds) is real-time")] private int _dayDuration = SECONDS_PER_DAY;
         [SerializeField, Range(0f, 23.99f)] private float _enableSunLightTime = 05.00f;
         [SerializeField, Range(0f, 23.99f)] private float _disableSunLightTime = 19.00f;
+        [SerializeField, Min(0f)] private float _dayDurModInputChange = 0.005f;
 
         [field: Header("Runtime")]
         [field: SerializeField, Range(0f, 23.99f)] public float CurrTime { get; set; } = DEFAULT_START_TIME;
+        [field: SerializeField, Min(0.001f)] public float DayDurationModifier { get; set; } = 1f;
 
         private void Awake() {
             if (_mainTimeSource) Main = this;
@@ -36,7 +38,10 @@ namespace World {
         }
 
         private void Update() {
-            CurrTime = (CurrTime + (Time.deltaTime / _dayDuration * 24f)) % 24f;
+            if (Input.GetKey(KeyCode.Alpha1)) DayDurationModifier += _dayDurModInputChange;
+            if (Input.GetKey(KeyCode.Alpha2)) DayDurationModifier = Mathf.Max(0.001f, DayDurationModifier - _dayDurModInputChange);
+
+            CurrTime = (CurrTime + (Time.deltaTime / (_dayDuration * DayDurationModifier) * 24f)) % 24f;
 
             if (!_light.enabled) {
                 if (CurrTime > _enableSunLightTime && CurrTime < _disableSunLightTime) _light.enabled = true;
