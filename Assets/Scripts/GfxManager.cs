a6096c2888cbb899fc290d742428d71e7ac36289
Paul Mieschke <p.mieschke@gmail.com>
2023-01-10 12:38:03 -0500

    GfxManager

---
+++ Assets/Scripts/GfxManager.cs
@@ -0,0 +1,48 @@
+using UnityEngine;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.HighDefinition;
+
+public class GfxManager : MonoBehaviour {
+    public static GfxManager Inst { get; private set; }
+
+    [Header("Setup")]
+    [SerializeField] private Volume _activeVolume = null;
+
+    [Header("Settings")]
+    [SerializeField] private bool _enableWater = true;
+    [SerializeField] private bool _enableClouds = true;
+
+    public bool EnableWater {
+        get => _enableWater;
+        set {
+            _enableWater = value;
+            _activeVolume.profile.components.Find(vc => vc.GetType() == typeof(WaterRendering)).active = _enableWater;
+        }
+    }
+
+    public bool EnableClouds {
+        get => _enableClouds;
+        set {
+            _enableClouds = value;
+            _activeVolume.profile.components.Find(vc => vc.GetType() == typeof(VolumetricClouds)).active = _enableClouds;
+        }
+    }
+
+    private void Awake() {
+        Inst = this;
+        if (!_activeVolume) Debug.LogWarning("GfxManager: no active volume assigned");
+    }
+
+    private void Update() {
+        if (Input.GetKeyDown(KeyCode.Alpha8)) EnableWater = !EnableWater;
+        if (Input.GetKeyDown(KeyCode.Alpha9)) EnableClouds = !EnableClouds;
+    }
+
+#if UNITY_EDITOR
+    private void OnValidate() {
+        if (!_activeVolume) return;
+        EnableWater = _enableWater;
+        EnableClouds = _enableClouds;
+    }
+#endif
+}
