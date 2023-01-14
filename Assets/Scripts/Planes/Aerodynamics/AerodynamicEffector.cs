08e4b2fd6e5ae06b024d0f372228cb8dba2329a6
Paul Mieschke <p.mieschke@gmail.com>
2023-01-13 22:00:49 -0500

    First flight attempts

--- Assets/Scripts/Planes/Aerodynamics/AerodynamicEffector.cs
+++ Assets/Scripts/Planes/Aerodynamics/AerodynamicEffector.cs
@@ -13,38 +13,81 @@ namespace Planes.Aerodynamics {
 
 #if UNITY_EDITOR
         [Header("Settings/Gizmos")]
+        [SerializeField, Min(0f)] private float _gizmoLocationSize = 0.2f;
         [SerializeField] private bool _gizmoShowExtendedChordLine = false;
         [SerializeField, Min(0f)] private float _gizmoExtendedChordLineLength = 1f;
         [SerializeField, Min(0f)] private float _gizmoAOALength = 1f;
+        [SerializeField, Min(0f)] private float _gizmoLiftLength = 1f;
 #endif
 
         private Rigidbody _rb;
 
-        private Vector3 wingChordLine;
-        private Vector3 relativeWind;
+        private Vector3 _oldPos;
+        private Vector3 _velocity;
+        private float _speed;
+        private float _IAS;
+
+        private Vector3 _wingChordLine;
+        private Vector3 _relativeWind;
+        private float _AOA;
+
+        private Vector3 _lift;
 
         private void Awake() {
             if (!_plane) Debug.LogWarning("AerodynamicEffector: no plane assigned");
             else _rb = _plane.GetComponent<Rigidbody>();
         }
 
+        private void Start() {
+            _oldPos = transform.position;
+        }
+
         private void FixedUpdate() {
-            wingChordLine = transform.forward;
-            relativeWind = Quaternion.Euler(0f, transform.eulerAngles.y, -transform.eulerAngles.z) * Vector3.forward; // TODO
+            UpdateVelocity();
+            UpdateAOA();
+            UpdateLift();
+            ApplyForces();
+        }
+
+        private void UpdateVelocity() {
+            _velocity = transform.position - _oldPos;
+            _oldPos = transform.position;
+
+            _speed = _velocity.magnitude;
+            _IAS = _speed * 1.94384f; // m/s to knots conversion
+        }
+
+        private void UpdateAOA() {
+            _wingChordLine = transform.forward;
+            _relativeWind = _velocity.normalized;
+            _AOA = Vector3.SignedAngle(_wingChordLine, _relativeWind, transform.right);
+        }
+
+        private void UpdateLift() {
+            _lift = transform.up * _speed * _baseLift * _AOALiftFactor.Evaluate(_AOA);
+        }
+
+        private void ApplyForces() {
+            _rb.AddForceAtPosition(_lift, transform.position, ForceMode.Force);
         }
 
 #if UNITY_EDITOR
         private void OnDrawGizmosSelected() {
+            Gizmos.color = Color.magenta;
+            Gizmos.DrawSphere(transform.position, _gizmoLocationSize);
+
             if (_gizmoShowExtendedChordLine) {
                 Gizmos.color = Color.white;
                 Gizmos.DrawLine(transform.position + transform.forward * _gizmoExtendedChordLineLength, transform.position - transform.forward * _gizmoExtendedChordLineLength);
             }
 
             if (!Application.isPlaying) return;
-            Gizmos.color = Color.green;
-            Gizmos.DrawLine(transform.position, transform.position + wingChordLine * _gizmoAOALength);
             Gizmos.color = Color.blue;
-            Gizmos.DrawLine(transform.position, transform.position + relativeWind * _gizmoAOALength);
+            Gizmos.DrawLine(transform.position, transform.position + _relativeWind * _gizmoAOALength);
+            Gizmos.color = Color.green;
+            Gizmos.DrawLine(transform.position, transform.position + _wingChordLine * _gizmoAOALength);
+
+            Gizmos.DrawLine(transform.position, transform.position + _lift * _gizmoLiftLength);
         }
 #endif
     }
